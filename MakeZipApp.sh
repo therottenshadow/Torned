ab070ea8e0ede84c8873dc23046a8c6d2ce8af4e
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-03-28 08:45:43 -0300

    Update script to fix bug

--- MakeZipApp.sh
+++ MakeZipApp.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-if [ -d ./src/.venv ]; then
-  source ./src/.venv/bin/activate
-  pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 pip install -U
-  pip install -r ./requirements.txt
-  python -m zipapp src --python="./src/.venv/bin/python3" --output="./Torned.pyz"
-else
-  python -m venv ./src/.venv
-  sleep 5
-  source ./src/.venv/bin/activate
-  pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 pip install -U
-  pip install -r ./requirements.txt
-  python -m zipapp src --python="./src/.venv/bin/python3" --output="./Torned.pyz"
-fi
-
+mkdir Pyz-Env
+cp main.py Pyz-Env/__main__.py
+pip install -r requirements.txt --target ./Pyz-Env
+python -m zipapp Pyz-Env -p "/usr/bin/env python3" -o Torned.pyz -c
+rm -r Pyz-Env
