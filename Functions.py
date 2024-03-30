9688879d479738032264f6859d116fd2ce9f4fbc
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-03-30 15:45:02 -0300

    Added sanitization functions

--- Functions.py
+++ Functions.py
@@ -1,3 +1,6 @@
+import re
+from TornAPIWrapper import TornApiWrapper
+
 ApiTranslate = {
   "name":"Name",
   "description":"Description",
@@ -11,7 +14,7 @@ ApiTranslate = {
   "circulation":"Circulation",
 }
 
-def SearchResultEmbedConstructor(ResultList):
+def SearchResultEmbedConstructor(ResultList: list):
   ResultingEmbed = {"Message":"","ImageUrl":""}
   for Result in ResultList:
     for ResultId in Result:
@@ -28,3 +31,23 @@ def SearchResultEmbedConstructor(ResultList):
           ResultingEmbed["Message"] += f'**{ApiTranslate[x]}**: {Result[ResultId][x]}\n'
       ResultingEmbed["Message"] += "\n"
   return ResultingEmbed
+
+def Sanitize():
+  def TornKey(DirtyString: str):
+    KeyRE = re.compile('[^A-Za-z0-9]')
+    if not(len(DirtyString) == 16):
+      raise ValueError("IncorrectLength")
+    elif KeyRE.match(DirtyString):
+      raise ValueError("IllegalCharacters")
+    else:
+      try:
+        TornObject = TornApiWrapper(api_key=DirtyString)
+        ApiData = TornObject.get_torn(selections=["timestamp"])
+        del TornObject
+        del ApiData
+      except:
+        raise ValueError("InvalidKey")
+  def SearchTerm(DirtyString: str):
+    SearchTermRE = re.compile("[^A-Za-z0-9-:&/+,!?'’ ]")
+    if SearchTermRE.match(DirtyString):
+      raise ValueError("IllegalCharacters")
