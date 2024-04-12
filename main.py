55bae5425f934e9a2119e4246e98f870ac0896ec
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-12 16:36:46 -0300

    0.1.1 (#39)
    * Added colors to embed objects

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ async def search(ctx,*,SearchString:str=None):
     await ctx.reply(
       files=[Images.RedCross(),Images.SearchIcon()],
       embed=discord.Embed(
-        description="It seems like you haven't given me any search parameters, wanna try that again?")
+        description="It seems like you haven't given me any search parameters, wanna try that again?",
+        color=Color.Red)
       .set_thumbnail(url=Images.ARedCross)
       .set_author(name="Item Search",icon_url=Images.ASearchIcon))
     return
@@ -32,7 +33,8 @@ async def search(ctx,*,SearchString:str=None):
       await ctx.reply(
         files=[Images.RedCross(),Images.SearchIcon()],
         embed=discord.Embed(
-          description="Your search contains illegal characters.\nYour input contains characters that are not allowed because they aren't part of any item's name")
+          description="Your search contains illegal characters.\nYour input contains characters that are not allowed because they aren't part of any item's name",
+        color=Color.Red)
         .set_thumbnail(url=Images.ARedCross)
         .set_author(name="Item Search",icon_url=Images.ASearchIcon))
       return
@@ -42,15 +44,16 @@ async def search(ctx,*,SearchString:str=None):
       await ctx.reply(
         file=Images.SearchIcon(),
         embed=discord.Embed(
-          description=Embed["Message"])
+          description=Embed["Message"],
+            color=Color.Cyan)
         .set_thumbnail(url=Embed["ImageUrl"])
         .set_author(name="Item Search Results",icon_url=Images.ASearchIcon))
     except KeyError:
       await ctx.reply(
         files=[Images.RedCross(),Images.SearchIcon()],
         embed=discord.Embed(
-          title="**Item Search**",
-          description="It seems like this item ID does not exist.")
+          description="It seems like this item ID does not exist.",
+          color=Color.Red)
         .set_thumbnail(url=Images.ARedCross)
         .set_author(name="Item Search",icon_url=Images.ASearchIcon))
   else:
@@ -60,7 +63,8 @@ async def search(ctx,*,SearchString:str=None):
         await ctx.reply(
           files=[Images.RedCross(),Images.SearchIcon()],
           embed=discord.Embed(
-            description="Seems like your search didn't bring up anything.\nAre you sure what you searched exists?? If it is something new it can take up to 6 hours for me to acknowledge it's existence.")
+            description="Seems like your search didn't bring up anything.\nAre you sure what you searched exists?? If it is something new it can take up to 6 hours for me to acknowledge it's existence.",
+            color=Color.Red)
           .set_thumbnail(url=Images.ARedCross)
           .set_author(name="Item Search",icon_url=Images.ASearchIcon))
       else:
@@ -68,14 +72,16 @@ async def search(ctx,*,SearchString:str=None):
         await ctx.reply(
           file=Images.SearchIcon(),
           embed=discord.Embed(
-            description=Embed["Message"])
+            description=Embed["Message"],
+            color=Color.Cyan)
           .set_thumbnail(url=Embed["ImageUrl"])
           .set_author(name="Item Search Results",icon_url=Images.ASearchIcon))
     else:
       await ctx.reply(
         files=[Images.RedCross(),Images.SearchIcon()],
         embed=discord.Embed(
-          description="Your search term is too short.\nI am not going to look through all that could turn up in that search.")
+          description="Your search term is too short.\nI am not going to look through all that could turn up in that search.",
+          color=Color.Red)
         .set_thumbnail(url=Images.ARedCross)
         .set_author(name="Item Search",icon_url=Images.ASearchIcon))
 
@@ -89,7 +95,8 @@ async def verify(ctx,ApiKey:str=None):
       file=Images.ShieldCheck(),
       embed=discord.Embed(
         title="**Verification Procedure**",
-        description="Hi,\n\nto complete the verification procedure please invoke the verify command in this DM and add a **public access** Torn API key, this key is viewable to the server owner, it is not needed to use any higher access key.\n\nTo create an API key, you need to go to your settings page in Torn, and under API Keys you will be able to create one, otherwise, you can use [this handy link](https://www.torn.com/preferences.php#tab=api?&step=addNewKey&title=TornedDiscordBot&type=1)\n\nHere is an example on how to invoke the command\n\n`/verify a1b2c3d4e5f6g7h8`")
+        description="Hi,\n\nto complete the verification procedure please invoke the verify command in this DM and add a **public access** Torn API key, this key is viewable to the server owner, it is not needed to use any higher access key.\n\nTo create an API key, you need to go to your settings page in Torn, and under API Keys you will be able to create one, otherwise, you can use [this handy link](https://www.torn.com/preferences.php#tab=api?&step=addNewKey&title=TornedDiscordBot&type=1)\n\nHere is an example on how to invoke the command\n\n`/verify a1b2c3d4e5f6g7h8`",
+        color=Color.Violet)
       .set_thumbnail(url=Images.AShieldCheck))
     return
   try:
@@ -99,7 +106,8 @@ async def verify(ctx,ApiKey:str=None):
       file=Images.ShieldCross(),
       embed=discord.Embed(
         title="**Verification Failed**",
-        description="Please check that the API key you entered is a valid key, it should only contain numbers and letters and be 16 characters in length")
+        description="Please check that the API key you entered is a valid key, it should only contain numbers and letters and be 16 characters in length",
+        color=Color.Red)
       .set_thumbnail(url=Images.AShieldCross))
     return
   Query = Db.SearchByDisId(AuthorId)
@@ -112,7 +120,8 @@ async def verify(ctx,ApiKey:str=None):
       file=Images.ShieldCross(),
       embed=discord.Embed(
         title="**Verification Failed**",
-        description="You are already registered on our database, you have no need to re-register")
+        description="You are already registered on our database, you have no need to re-register",
+        color=Color.Red)
       .set_thumbnail(url=Images.AShieldCross))
     return
   InvokingUser = User(DiscordUserId=AuthorId,TornApiKey=ApiKey)
@@ -123,7 +132,8 @@ async def verify(ctx,ApiKey:str=None):
     file=Images.GreenShieldCheck(),
     embed=discord.Embed(
       title="**Verification Successful**",
-      description="You have been successfully verified on our database, now you should have been assigned appropiate roles and be able to use all of my services")
+      description="You have been successfully verified on our database, now you should have been assigned appropiate roles and be able to use all of my services",
+      color=Color.Green)
     .set_thumbnail(url=Images.AGreenShieldCheck))
 
 @Bot.command()
@@ -132,7 +142,8 @@ async def price(ctx,*,SearchString:str=None):
     await ctx.reply(
       files=[Images.RedCross(),Images.PriceIcon()],
       embed=discord.Embed(
-        description="Please introduce the name or ID number of an item to get it's average item market and bazaar prices.\n\nBelow is an example\n`/price Hammer`")
+        description="Please introduce the name or ID number of an item to get it's average item market and bazaar prices.\n\nBelow is an example\n`/price Hammer`",
+        color=Color.Red)
       .set_thumbnail(url=Images.ARedCross)
       .set_author(name="Price Check",icon_url=Images.APriceIcon))
     return
@@ -144,7 +155,8 @@ async def price(ctx,*,SearchString:str=None):
       await ctx.reply(
         files=[Images.RedCross(),Images.PriceIcon()],
         embed=discord.Embed(
-          description="Your search contains illegal characters.\n\nYour input contains characters that are not allowed because they aren't part of any item's name")
+          description="Your search contains illegal characters.\n\nYour input contains characters that are not allowed because they aren't part of any item's name",
+          color=Color.Red)
         .set_thumbnail(url=Images.ARedCross)
         .set_author(name="Price Check",icon_url=Images.APriceIcon))
       return
@@ -154,7 +166,8 @@ async def price(ctx,*,SearchString:str=None):
     await ctx.reply(
       files=[Images.RedCross(),Images.PriceIcon()],
       embed=discord.Embed(
-        description="It looks like you haven't yet verified, please run /verify to setup an API key with which you can perform a price check")
+        description="It looks like you haven't yet verified, please run /verify to setup an API key with which you can perform a price check",
+        color=Color.Red)
       .set_thumbnail(url=Images.ARedCross)
       .set_author(name="Price Check",icon_url=Images.APriceIcon))
     return
@@ -168,7 +181,8 @@ async def price(ctx,*,SearchString:str=None):
       await ctx.reply(
         files=[Images.RedCross(),Images.PriceIcon()],
         embed=discord.Embed(
-          description="It seems like this item ID does not exist.")
+          description="It seems like this item ID does not exist.",
+          color=Color.Red)
         .set_thumbnail(url=Images.ARedCross)
         .set_author(name="Price Check",icon_url=Images.APriceIcon))
       return
@@ -177,7 +191,8 @@ async def price(ctx,*,SearchString:str=None):
     await ctx.reply(
       file=Images.PriceIcon(),
       embed=discord.Embed(
-        description=Desc)
+        description=Desc,
+        color=Color.Yellow)
       .set_thumbnail(url=InfoQuery[[x for x in InfoQuery][0]]["image"])
       .set_author(name="Price Check Results",icon_url=Images.APriceIcon))
     return
@@ -188,7 +203,8 @@ async def price(ctx,*,SearchString:str=None):
         await ctx.reply(
           files=[Images.RedCross(),Images.PriceIcon()],
           embed=discord.Embed(
-            description="Are you sure the item you searched exists?? If it is something new it can take up to 6 hours for me to acknowledge it's existence.")
+            description="Are you sure the item you searched exists?? If it is something new it can take up to 6 hours for me to acknowledge it's existence.",
+            color=Color.Red)
           .set_thumbnail(url=Images.ARedCross)
           .set_author(name="Price Check",icon_url=Images.APriceIcon))
       else:
@@ -199,14 +215,16 @@ async def price(ctx,*,SearchString:str=None):
         await ctx.reply(
           file=Images.PriceIcon(),
           embed=discord.Embed(
-            description=Desc)
+            description=Desc,
+            color=Color.Yellow)
           .set_thumbnail(url=SearchResult[0][SearchItemId]["image"])
           .set_author(name="Price Check Results",icon_url=Images.APriceIcon))
     else:
       await ctx.reply(
         files=[Images.RedCross(),Images.PriceIcon()],
         embed=discord.Embed(
-          description="Sorry, but the term you gave me is too undescriptive and is unlikely to return the wanted item's price, please enter at least 3 characters, or even better, the item's ID number")
+          description="Sorry, but the term you gave me is too undescriptive and is unlikely to return the wanted item's price, please enter at least 3 characters, or even better, the item's ID number",
+          color=Color.Red)
         .set_thumbnail(url=Images.ARedCross)
         .set_author(name="Price Check",icon_url=Images.APriceIcon))
 
@@ -218,7 +236,8 @@ async def points(ctx):
     await ctx.reply(
       files=[Images.RedCross()],
       embed=discord.Embed(
-        description="It looks like you haven't yet verified, please run /verify to setup an API key with which you can perform a points price check")
+        description="It looks like you haven't yet verified, please run /verify to setup an API key with which you can perform a points price check",
+        color=Color.Red)
       .set_thumbnail(url=Images.ARedCross)
       .set_author(name="Points Price Check",icon_url=Images.ARedCross))
     return
@@ -239,7 +258,8 @@ Please remember this is not an accurate reading of the points market, the volume
   await ctx.reply(
     files=[Images.PointsIcon(),Images.TornedIcon()],
     embed=discord.Embed(
-      description=Message)
+      description=Message,
+      color=Color.Yellow)
     .set_thumbnail(url=Images.APointsIcon)
     .set_author(name="Points Price Check",icon_url=Images.ATornedIcon))
 
