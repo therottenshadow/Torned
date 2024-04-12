739e219c7cea01d2c45bfb40b1c9a0f59d324cc7
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-12 14:34:00 -0300

    Added /points Made use of new variables in Images class to be able to change icons much more easily

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ async def search(ctx,*,SearchString:str=None):
       files=[Images.RedCross(),Images.SearchIcon()],
       embed=discord.Embed(
         description="It seems like you haven't given me any search parameters, wanna try that again?")
-      .set_thumbnail(url="attachment://RedCross.png")
-      .set_author(name="Item Search",icon_url="attachment://SearchIcon.png"))
+      .set_thumbnail(url=Images.ARedCross)
+      .set_author(name="Item Search",icon_url=Images.ASearchIcon))
     return
   SearchString = unidecode(SearchString)
   try:
@@ -33,8 +33,8 @@ async def search(ctx,*,SearchString:str=None):
         files=[Images.RedCross(),Images.SearchIcon()],
         embed=discord.Embed(
           description="Your search contains illegal characters.\nYour input contains characters that are not allowed because they aren't part of any item's name")
-        .set_thumbnail(url="attachment://RedCross.png")
-        .set_author(name="Item Search",icon_url="attachment://SearchIcon.png"))
+        .set_thumbnail(url=Images.ARedCross)
+        .set_author(name="Item Search",icon_url=Images.ASearchIcon))
       return
   if SearchString.isdigit():
     try:
@@ -44,15 +44,15 @@ async def search(ctx,*,SearchString:str=None):
         embed=discord.Embed(
           description=Embed["Message"])
         .set_thumbnail(url=Embed["ImageUrl"])
-        .set_author(name="Item Search Results",icon_url="attachment://SearchIcon.png"))
+        .set_author(name="Item Search Results",icon_url=Images.ASearchIcon))
     except KeyError:
       await ctx.reply(
         files=[Images.RedCross(),Images.SearchIcon()],
         embed=discord.Embed(
           title="**Item Search**",
           description="It seems like this item ID does not exist.")
-        .set_thumbnail(url="attachment://RedCross.png")
-        .set_author(name="Item Search",icon_url="attachment://SearchIcon.png"))
+        .set_thumbnail(url=Images.ARedCross)
+        .set_author(name="Item Search",icon_url=Images.ASearchIcon))
   else:
     if len(SearchString) > 2:
       SearchResult = ItemDb.SearchByString(SearchString)
@@ -61,8 +61,8 @@ async def search(ctx,*,SearchString:str=None):
           files=[Images.RedCross(),Images.SearchIcon()],
           embed=discord.Embed(
             description="Seems like your search didn't bring up anything.\nAre you sure what you searched exists?? If it is something new it can take up to 6 hours for me to acknowledge it's existence.")
-          .set_thumbnail(url="attachment://RedCross.png")
-          .set_author(name="Item Search",icon_url="attachment://SearchIcon.png"))
+          .set_thumbnail(url=Images.ARedCross)
+          .set_author(name="Item Search",icon_url=Images.ASearchIcon))
       else:
         Embed = Functions.SearchResultEmbedConstructor(SearchResult[:3])
         await ctx.reply(
@@ -70,14 +70,14 @@ async def search(ctx,*,SearchString:str=None):
           embed=discord.Embed(
             description=Embed["Message"])
           .set_thumbnail(url=Embed["ImageUrl"])
-          .set_author(name="Item Search Results",icon_url="attachment://SearchIcon.png"))
+          .set_author(name="Item Search Results",icon_url=Images.ASearchIcon))
     else:
       await ctx.reply(
         files=[Images.RedCross(),Images.SearchIcon()],
         embed=discord.Embed(
           description="Your search term is too short.\nI am not going to look through all that could turn up in that search.")
-        .set_thumbnail(url="attachment://RedCross.png")
-        .set_author(name="Item Search",icon_url="attachment://SearchIcon.png"))
+        .set_thumbnail(url=Images.ARedCross)
+        .set_author(name="Item Search",icon_url=Images.ASearchIcon))
 
 @Bot.command()
 async def verify(ctx,ApiKey:str=None):
@@ -90,7 +90,7 @@ async def verify(ctx,ApiKey:str=None):
       embed=discord.Embed(
         title="**Verification Procedure**",
         description="Hi,\n\nto complete the verification procedure please invoke the verify command in this DM and add a **public access** Torn API key, this key is viewable to the server owner, it is not needed to use any higher access key.\n\nTo create an API key, you need to go to your settings page in Torn, and under API Keys you will be able to create one, otherwise, you can use [this handy link](https://www.torn.com/preferences.php#tab=api?&step=addNewKey&title=TornedDiscordBot&type=1)\n\nHere is an example on how to invoke the command\n\n`/verify a1b2c3d4e5f6g7h8`")
-      .set_thumbnail(url="attachment://ShieldCheck.png"))
+      .set_thumbnail(url=Images.AShieldCheck))
     return
   try:
     Functions.SanitizeTornKey(ApiKey)
@@ -100,7 +100,7 @@ async def verify(ctx,ApiKey:str=None):
       embed=discord.Embed(
         title="**Verification Failed**",
         description="Please check that the API key you entered is a valid key, it should only contain numbers and letters and be 16 characters in length")
-      .set_thumbnail(url="attachment://ShieldCross.png"))
+      .set_thumbnail(url=Images.AShieldCross))
     return
   Query = Db.SearchByDisId(AuthorId)
   if Query is None:
@@ -113,7 +113,7 @@ async def verify(ctx,ApiKey:str=None):
       embed=discord.Embed(
         title="**Verification Failed**",
         description="You are already registered on our database, you have no need to re-register")
-      .set_thumbnail(url="attachment://ShieldCross.png"))
+      .set_thumbnail(url=Images.AShieldCross))
     return
   InvokingUser = User(DiscordUserId=AuthorId,TornApiKey=ApiKey)
   InvokingUser.Populate()
@@ -124,7 +124,7 @@ async def verify(ctx,ApiKey:str=None):
     embed=discord.Embed(
       title="**Verification Successful**",
       description="You have been successfully verified on our database, now you should have been assigned appropiate roles and be able to use all of my services")
-    .set_thumbnail(url="attachment://GreenShieldCheck.png"))
+    .set_thumbnail(url=Images.AGreenShieldCheck))
 
 @Bot.command()
 async def price(ctx,*,SearchString:str=None):
@@ -133,8 +133,8 @@ async def price(ctx,*,SearchString:str=None):
       files=[Images.RedCross(),Images.PriceIcon()],
       embed=discord.Embed(
         description="Please introduce the name or ID number of an item to get it's average item market and bazaar prices.\n\nBelow is an example\n`/price Hammer`")
-      .set_thumbnail(url="attachment://RedCross.png")
-      .set_author(name="Price Check",icon_url="attachment://PriceIcon.png"))
+      .set_thumbnail(url=Images.ARedCross)
+      .set_author(name="Price Check",icon_url=Images.APriceIcon))
     return
   SearchString = unidecode(SearchString).lower()
   try:
@@ -145,8 +145,8 @@ async def price(ctx,*,SearchString:str=None):
         files=[Images.RedCross(),Images.PriceIcon()],
         embed=discord.Embed(
           description="Your search contains illegal characters.\n\nYour input contains characters that are not allowed because they aren't part of any item's name")
-        .set_thumbnail(url="attachment://RedCross.png")
-        .set_author(name="Price Check",icon_url="attachment://PriceIcon.png"))
+        .set_thumbnail(url=Images.ARedCross)
+        .set_author(name="Price Check",icon_url=Images.APriceIcon))
       return
   AuthorId = ctx.message.author.id
   Query = Db.SearchByDisId(AuthorId)
@@ -155,8 +155,8 @@ async def price(ctx,*,SearchString:str=None):
       files=[Images.RedCross(),Images.PriceIcon()],
       embed=discord.Embed(
         description="It looks like you haven't yet verified, please run /verify to setup an API key with which you can perform a price check")
-      .set_thumbnail(url="attachment://RedCross.png")
-      .set_author(name="Price Check",icon_url="attachment://PriceIcon.png"))
+      .set_thumbnail(url=Images.ARedCross)
+      .set_author(name="Price Check",icon_url=Images.APriceIcon))
     return
   else:
     TornApi = TornApiWrapper(api_key=Query.TornApiKey)
@@ -169,8 +169,8 @@ async def price(ctx,*,SearchString:str=None):
         files=[Images.RedCross(),Images.PriceIcon()],
         embed=discord.Embed(
           description="It seems like this item ID does not exist.")
-        .set_thumbnail(url="attachment://RedCross.png")
-        .set_author(name="Price Check",icon_url="attachment://PriceIcon.png"))
+        .set_thumbnail(url=Images.ARedCross)
+        .set_author(name="Price Check",icon_url=Images.APriceIcon))
       return
     Results = Functions.PriceAverageCalculator(DataQuery)
     Desc = Functions.PriceEmbedConstructor(InfoQuery[[x for x in InfoQuery][0]]['name'],[x for x in InfoQuery][0],Results)
@@ -179,7 +179,7 @@ async def price(ctx,*,SearchString:str=None):
       embed=discord.Embed(
         description=Desc)
       .set_thumbnail(url=InfoQuery[[x for x in InfoQuery][0]]["image"])
-      .set_author(name="Price Check Results",icon_url="attachment://PriceIcon.png"))
+      .set_author(name="Price Check Results",icon_url=Images.APriceIcon))
     return
   else:
     if len(SearchString) > 2:
@@ -189,8 +189,8 @@ async def price(ctx,*,SearchString:str=None):
           files=[Images.RedCross(),Images.PriceIcon()],
           embed=discord.Embed(
             description="Are you sure the item you searched exists?? If it is something new it can take up to 6 hours for me to acknowledge it's existence.")
-          .set_thumbnail(url="attachment://RedCross.png")
-          .set_author(name="Price Check",icon_url="attachment://PriceIcon.png"))
+          .set_thumbnail(url=Images.ARedCross)
+          .set_author(name="Price Check",icon_url=Images.APriceIcon))
       else:
         SearchItemId = [x for x in SearchResult[0]][0]
         DataQuery = TornApi.get_market(SearchItemId,selections=["bazaar","itemmarket"])
@@ -201,14 +201,47 @@ async def price(ctx,*,SearchString:str=None):
           embed=discord.Embed(
             description=Desc)
           .set_thumbnail(url=SearchResult[0][SearchItemId]["image"])
-          .set_author(name="Price Check Results",icon_url="attachment://PriceIcon.png"))
+          .set_author(name="Price Check Results",icon_url=Images.APriceIcon))
     else:
       await ctx.reply(
         files=[Images.RedCross(),Images.PriceIcon()],
         embed=discord.Embed(
           description="Sorry, but the term you gave me is too undescriptive and is unlikely to return the wanted item's price, please enter at least 3 characters, or even better, the item's ID number")
-        .set_thumbnail(url="attachment://RedCross.png")
-        .set_author(name="Price Check",icon_url="attachment://PriceIcon.png"))
+        .set_thumbnail(url=Images.ARedCross)
+        .set_author(name="Price Check",icon_url=Images.APriceIcon))
+
+@Bot.command()
+async def points(ctx):
+  AuthorId = ctx.message.author.id
+  Query = Db.SearchByDisId(AuthorId)
+  if Query is None:
+    await ctx.reply(
+      files=[Images.RedCross()],
+      embed=discord.Embed(
+        description="It looks like you haven't yet verified, please run /verify to setup an API key with which you can perform a points price check")
+      .set_thumbnail(url=Images.ARedCross)
+      .set_author(name="Points Price Check",icon_url=Images.ARedCross))
+    return
+  else:
+    TornApi = TornApiWrapper(api_key=Query.TornApiKey)
+  DataDict = TornApi.get_market(0,selections=["pointsmarket"])["pointsmarket"]
+  Data = []
+  for x in DataDict:
+    Data.append(DataDict[x])
+  Data = Data[:5]
+  Message = "Here is the data of the first 5 sell orders for points:\n\n"
+  for x in Data:
+    Message += f'• Price/Point: `${x["cost"]:,.0f}` Quantity: `{x["quantity"]}` Total Price: `${x["total_cost"]:,.0f}`\n'
+  Message += """
+[Here is a link to go directly to the Points Market](https://www.torn.com/pmarket.php)
+
+Please remember this is not an accurate reading of the points market, the volume of sells is so high you might not get to buy these specific orders, this is a mere estimation"""
+  await ctx.reply(
+    files=[Images.PointsIcon(),Images.TornedIcon()],
+    embed=discord.Embed(
+      description=Message)
+    .set_thumbnail(url=Images.APointsIcon)
+    .set_author(name="Points Price Check",icon_url=Images.ATornedIcon))
 
 @Bot.command()
 async def ping(ctx):
