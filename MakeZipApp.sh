7cffdcb43530ef756e2b6a1c63d34733df26f6a3
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-13 16:46:25 -0300

    Update ZipApp script

--- MakeZipApp.sh
+++ MakeZipApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir Pyz-Env
-cp -r ./*.py resources Pyz-Env/
-pip install -r requirements.txt --target ./Pyz-Env
+cp -r ./src/* Pyz-Env/
+pip install --isolated --python "/usr/bin/env python3" -r requirements.txt --target ./Pyz-Env
 python -m zipapp Pyz-Env -p "/usr/bin/env python3" -o Torned.pyz -c
 rm -r Pyz-Env
