0f49242001ab503eac575874db56426f2f9b768b
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-14 00:32:34 -0300

    Fix cog_template's indentation

--- cog_template.py
+++ cog_template.py
@@ -1,12 +1,12 @@
 from discord.ext import commands
 
 class My_Cog(commands.Cog, name='Your Cog Name'):
-    def __init__(self, bot):
-        self.bot = bot
+  def __init__(self, bot):
+    self.bot = bot
 
-    @commands.command(name='test' , brief='This is the brief description', description='This is the full description')
-    async def test(ctx):
-        await ctx.send('test')
+  @commands.command(name='test' , brief='This is the brief description', description='This is the full description')
+  async def test(ctx):
+    await ctx.send('test')
 
 def setup(bot):
-    bot.add_cog(My_Cog(bot))
+  bot.add_cog(My_Cog(bot))
