e368acc5aced6f438d62b40f2ea6f3e8ddf3c1eb
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-14 11:25:15 -0300

    Move cogs into a cogs directory instead of ZipApp root directory

--- MakeZipApp.sh
+++ MakeZipApp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir Pyz-Env
-cp -r ./src/*.py ./src/resources ./src/cogs/*.py Pyz-Env/
+cp -r ./src/*.py ./src/resources Pyz-Env/
+cp ./src/cogs/*.py Pyz-Env/
 pip install --isolated -r requirements.txt --target ./Pyz-Env
 python -m zipapp Pyz-Env -p "/usr/bin/env python3" -o Torned.pyz -c
 rm -r Pyz-Env
