744a1eec340b55e3ecb337f8d5e35c6408bcbbcd
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-13 23:48:11 -0300

    Add a template for creating a cog file extracted from https://stackoverflow.com/questions/65724180/how-to-add-categories-to-commands-discord-py

---
+++ cog_template.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+class My_Cog(commands.Cog, name='Your Cog Name'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='test' , brief='This is the brief description', description='This is the full description')
+    async def test(ctx):
+        await ctx.send('test')
+
+def setup(bot):
+    bot.add_cog(My_Cog(bot))
