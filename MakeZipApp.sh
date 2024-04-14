7a17d419a22c64c3704849335e5f53bb01c45141
therottenshadow <84259653+therottenshadow@users.noreply.github.com>
2024-04-14 18:57:21 -0300

    Fix cogs inside ZipApp

--- MakeZipApp.sh
+++ MakeZipApp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir --parents Pyz-Env/cogs
 cp LICENSE README.md Pyz-Env/
-cp -r ./src/*.py ./src/resources Pyz-Env/
+cp ./src/*.py Pyz-Env/
+cp -r ./src/resources Pyz-Env
 cp ./src/cogs/*.py Pyz-Env/cogs
 pip install --isolated -r requirements.txt --target ./Pyz-Env
 python -m zipapp Pyz-Env -p "/usr/bin/env python3" -o Torned.pyz -c
--- src/__main__.py
+++ src/__main__.py
@@ -5,6 +5,12 @@ from discord.ext import commands
 
 from Config import Config
 
+CogsList = [
+  "discord_cog",
+  "torn_cog",
+  "users_cog",
+  "bot_cog"
+]
 
 Intents = discord.Intents.default()
 Intents.message_content = True
@@ -13,9 +19,8 @@ Bot = commands.Bot(command_prefix=Config.Bot["Command Prefix"],intents=Intents)
 
 @Bot.event
 async def on_ready():
-  for file in listdir("cogs/"):
-    if file.endswith(".py"):
-      await Bot.load_extension(f"cogs.{file[:-3]}")
+  for Cog in CogsList:
+    await Bot.load_extension(f"cogs.{Cog}")
 
 
 Bot.run(Config.Bot["Discord Token"])
